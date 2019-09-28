58f2a35d48bc6f9ca765572835fd44f805cbc9cb
Craig Iturbe <citurbe@gmail.com>
2019-09-28 14:15:47 -0400

    add ability to save and persist state

--- src/store.js
+++ src/store.js
@@ -1,9 +1,27 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './redux/rootReducer';
 const composeEnhancers = composeWithDevTools({});
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('skinnerBox');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const persistedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
